a54233d467e3004de90e1922954737b237aad260
syafiq <ahmadsyafiqkamil@gmail.com>
2019-03-06 15:19:14 +0700

    account/view/login

--- account/views.py
+++ account/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
+from django.contrib.auth import authenticate
 
 # Create your views here.
 def login(request) :
-    return render(request,'account/login.html')
+    if request.method == 'POST':
+
+        user = auth.authenticate(username = request.POST['username'],password = request.POST['password'] )
+        if user is not None:
+            auth.login(request,user)
+            return redirect('dashboard')
+        else:
+            return render(request,'account/login.html',{'error':'Cek Username dan Password anda'})
+    else:
+        return render(request,'account/login.html')
 
 def register(request) :
     if request.method == 'POST':
@@ -20,3 +30,8 @@ def register(request) :
             return render(request,'account/register.html',{'errorps':'password tidak sama'})
     else :
         return render(request,'account/register.html')
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('login')
--- survey/urls.py
+++ survey/urls.py
@@ -3,10 +3,13 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-from account import views
+# from account import views
+# from dashboard import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('account.urls')),
     path('',include('dashboard.urls')),
+    # path('dashboard/',views.dashboard, name= "dashboard"),
+
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
