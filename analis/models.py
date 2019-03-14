756a357b273df749fd84c0a0b0ae7b25d3c81715
syafiq <ahmadsyafiqkamil@gmail.com>
2019-03-14 12:23:11 +0700

    migrate model analis

--- analis/models.py
+++ analis/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
 # Create your models here.
+class Proyek(models.Model):
+    nama_proyek = models.CharField(max_length = 255)
+    deskripsi_proyek = models.TextField()
+    # id_perangkat = models.ForeignKey(Perangkat, on_delete=models.CASCADE)
+
+
+class Organisasi(models.Model):
+    nama_organisasi = models.CharField(max_length = 255)
+    # id_proyek = models.ForeignKey(Proyek, on_delete=models.CASCADE)
+
+class Perangkat(models.Model):
+    survey = JSONField()
